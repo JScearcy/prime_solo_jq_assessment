81f36e04509913596cdf9b2e372c4a7d54b2d72b
JScearcy <jacobscearcy@gmail.com>
2015-08-19 16:35:51 -0500

    Second commit, less code, more features - it toggles now

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(function(){
   var lineCount = 0;
   var $targetDiv = $('#TargetDiv');
-
+  // create div
   $('#GenButton').on('click', function(){
     lineCount++
     $(this).blur();
@@ -10,29 +10,21 @@ $(function(){
     $but1 = $('<button>');
     $but2 = $('<button>');
     $but1.text('Change Color')
-    $but1.attr('class', 'js-changeColor divBut');
+    $but1.attr({class: 'js-changeColor divBut'});
     $but2.text('Remove');
-    $but2.attr('class', 'js-delete divBut');
+    $but2.attr({class: 'js-delete divBut'});
     $p.text('Line #: ' + lineCount);
-    // $p.append($but1);
-    // $p.append($but2);
     $div.append($p);
     $div.append($but1);
-    $div.append($but2);
+    $div.append($but2)
     $targetDiv.append($div);
-    $div.attr('class', 'normal');
-    deleteDiv();
-    changeDivColor();
   })
-
-  function deleteDiv(){
-    $('.js-delete').on('click', function(){
-      $(this).parent().remove();
-    })
-  }
-  function changeDivColor(){
-    $('.js-changeColor').on('click', function(){
-      $(this).parent().addClass('different');
-    })
-  }
+  //change color by toggling class
+  $targetDiv.on('click', '.js-changeColor', function(){
+        $(this).parent().toggleClass('different');
+  })
+  //delete div
+  $targetDiv.on('click', '.js-delete', function(){
+    $(this).parent().remove();
+  })
 })
