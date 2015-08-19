03ec35f88b423a221f86438d25f82f5d245a5f0d
JScearcy <jacobscearcy@gmail.com>
2015-08-19 16:46:20 -0500

    Final commit, colors

--- scripts.js
+++ scripts.js
@@ -9,19 +9,20 @@ $(function(){
     $p = $('<p>');
     $but1 = $('<button>');
     $but2 = $('<button>');
-    $but1.text('Change Color')
+    $but1.text('Change Color');
     $but1.attr({class: 'js-changeColor divBut'});
     $but2.text('Remove');
     $but2.attr({class: 'js-delete divBut'});
     $p.text('Line #: ' + lineCount);
     $div.append($p);
     $div.append($but1);
-    $div.append($but2)
+    $div.append($but2);
     $targetDiv.append($div);
   })
   //change color by toggling class
   $targetDiv.on('click', '.js-changeColor', function(){
-        $(this).parent().toggleClass('different');
+      $(this).parent().toggleClass('different');
+      $(this).blur();
   })
   //delete div
   $targetDiv.on('click', '.js-delete', function(){
